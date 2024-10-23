0a2d0dab888db546dea2a0a673f1f19cb32c16fb
Joty72 <jotikakhatri2@gmail.com>
2024-10-23 00:44:52 -0700

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+  int num=10;
+  int sum=0;
+  for(int i=1; i<11;i++){
+      sum=sum+i;
+   
+      
+  }
+     System.out.println(sum);
+    }
+}
